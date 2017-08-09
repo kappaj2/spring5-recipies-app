d21ca9347597a530b5388f97edbbbaf82fb27a5e
Andre Kapp <andrekapp@estalea.com>
2017-08-09 19:12:02 +0200

    Added customer query in repositories and data.sql file to load data

--- src/main/java/za/co/ajk/recipe/controllers/IndexController.java
+++ src/main/java/za/co/ajk/recipe/controllers/IndexController.java
@@ -1,10 +1,25 @@
 package za.co.ajk.recipe.controllers;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import za.co.ajk.recipe.domain.Category;
+import za.co.ajk.recipe.domain.UnitOfMeasure;
+import za.co.ajk.recipe.repositories.CategoryRepository;
+import za.co.ajk.recipe.repositories.UnitOfMeasureRepository;
+
 @Controller     
 public class IndexController {
+    
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+    
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
     
     /**
      * Map multiple request endpoints. Return index.html thymeleaf file.
@@ -13,6 +28,13 @@ public class IndexController {
     @RequestMapping({"", "/", "index"})
     public String getIndexPage(){
         System.out.println("Requested index 34");
+    
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+    
+        System.out.println("Cat id is : "+categoryOptional.get().getId());
+        System.out.println("UOM id is : "+unitOfMeasureOptional.get().getId());
+      
         return "index";
     }
 
